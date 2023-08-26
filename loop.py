8a85499e382bff98ae78112869ed8cbe6004db9e
Febri Nur Rohman <nurrohman.febri@gmail.com>
2023-08-26 16:02:53 +0700

    commit loop.py

---
+++ loop.py
@@ -0,0 +1,10 @@
+count_egg = 10
+
+for i in range(1, count_egg + 1):
+    print(f"Egg {i}")
+
+counted_egg = 1
+
+while count_egg >= counted_egg:
+    print(f"Remaining egg {counted_egg}")
+    counted_egg += 1
