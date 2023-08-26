f80abc1d2d60c1fd846574348ebb66c83135ad85
Febri Nur Rohman <nurrohman.febri@gmail.com>
2023-08-26 15:46:40 +0700

    commit condition.py

---
+++ condition.py
@@ -0,0 +1,12 @@
+count_milk = 3123
+count_egg = 123
+
+if count_milk > 0:
+    print("Buy 1 bottle of milk")
+
+    if count_egg > 0:
+        print("Buy 6 eggs")
+    else:
+        print("Buy no egg")
+else:
+    print("Buy no milk")
